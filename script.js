79d3f67e8c942aa91b6d49327bd3e406d8c76614
Boat"   Sasipatr Siripairoj <125846563+sspsi77@users.noreply.github.com>
2023-03-13 16:38:01 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,25 @@
+const addNumbers = function (a,b) {
+    return a + b;
+}
+console.log(addNumbers(6, 2));
+
+const subtractNumbers = function (a,b) {
+    return a - b;
+}
+console.log(subtractNumbers(6, 2));
+
+const multiplyNumber = function (a, b) {
+    return a * b;
+}
+console.log(multiplyNumber(6, 2));
+
+const divideNumber = function (a, b) {
+    return a / b;
+}
+console.log(divideNumber(6, 2));
+
+const createText = function (firstName, location, hobby) {
+    console.log("Hi, my name is " + firstName + " I live in " + location + " and enjoy "+hobby);
+}
+
+createText('Sasipatr', 'Thailand', 'listening to music.');
